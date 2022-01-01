ff3d64dbdbd4d731769b6d94dc451853f0390ab1
Abdulrahman Khalid <47614266+anthibo@users.noreply.github.com>
2022-01-01 20:48:48 +0200

    fixing saleController.store()

--- app/Http/Controllers/salecontroller.php
+++ app/Http/Controllers/salecontroller.php
@@ -23,27 +23,32 @@ class salecontroller extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
+     * batl l3b aya baba
      */
-    public function store(Request $request)
+    public function store(Request $requests)
     {
-        $request->validate([
-            'product_id' => 'required',
-            'quantity' => 'required',
-        ]);
+        $requestsBody = json_decode($requests->getContent());
 
-        $product_id = $request->input('product_id');
-        $quantity = $request->input('quantity');
-        $product = Product::find($product_id);
-        if ($quantity > $product->quantity) {
-            return response()->json(['message' => 'Invalid quantity'], 202);
+        foreach ($requestsBody as $request) {
+            // $request->validate([
+            //     'product_id' => 'required',
+            //     'quantity' => 'required',
+            // ]);
+
+            $product_id = $request->product_id;
+            $quantity = $request->quantity;
+            $product = Product::find($product_id);
+            if ($quantity > $product->quantity) {
+                return response()->json(['message' => 'Invalid quantity'], 202);
+            }
+            $product->quantity = $product->quantity - $quantity;
+            $product->save();
+            sales::create([
+                'product_id' => $product_id,
+                'quantity' => $quantity,
+                'price' => $product->price
+            ]);
         }
-        $product->quantity = $product->quantity - $quantity;
-        $product->save();
-        sales::create([
-            'product_id' => $product_id,
-            'quantity' => $quantity,
-            'price' => $product->price
-        ]);
         return response()->json($product, 202);
     }
 }
